1f20a4ed217f788a7f538bbd0defbcb4d5c393f0
Konoyaro <chepel1997@gmail.com>
2024-04-12 00:30:24 +0600

    added shop page

--- gulpfile.js
+++ gulpfile.js
@@ -80,6 +80,7 @@ function scripts() {
     "node_modules/slick-carousel/slick/slick.js",
     "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
     "node_modules/rateyo/src/jquery.rateyo.js",
+    "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
     "app/js/main.js",
   ]) // Выбор всех JavaScript файлов
     .pipe(concat("main.min.js")) // Объединение файлов
